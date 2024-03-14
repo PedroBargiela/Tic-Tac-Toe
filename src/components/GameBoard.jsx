30fdefca5c88237577996b453e20a3a83314f40c
Pedro <pedrobargiela@gmail.com>
2024-03-14 17:15:18 +0100

    Final Game

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,39 +1,17 @@
-const initailGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
-];
-
-export default function GameBoard( {onSelectSquare, turns}) {
-    let gameBoard = initailGameBoard;
-
-    for (const turn of turns) {
-        const { square, player} = turn;
-        const { row, col } = square;
-
-        gameBoard[row][col] = player;
-    }
-
-    // const [gameBoard, setGameBoard] = useState(initailGameBoard);
-
-    // function handleSelectSquare(rowIndex, colIndex) {
-    //     setGameBoard((prevGameBoard) => {
-    //         const updateBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-    //         updateBoard[rowIndex][colIndex] = activePlayerSymbol;
-    //         return updateBoard;
-    //     } );
-
-    //     onSelectSquare();
-    //}
-
+export default function GameBoard( {onSelectSquare, board}) {
     return (
         <ol id="game-board">
-            {gameBoard.map((row, rowIndex) => ( 
+            {board.map((row, rowIndex) => ( 
                 <li key={rowIndex}>
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
                             <li key={colIndex}>
-                                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                                <button 
+                                    onClick={() => onSelectSquare(rowIndex, colIndex)} 
+                                    disabled={playerSymbol !== null}
+                                >
+                                    {playerSymbol}
+                                </button>
                             </li>
                         ))}
                     </ol>
